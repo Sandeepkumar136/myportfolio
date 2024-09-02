8109919a97a0baecdfecf76b61cd650ca066287d
Sandeepkumar136 <ksundip750@gmail.com>
2024-09-02 23:12:29 +0530

    Completed but media quirey not completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,76 @@ const Home = () => {
     <div>
       <Loader/>
       <HomeTyping/>
+      <h2 className="home-heading">
+        updated informations
+      </h2>
       <div className="h-card-container">
-        <div className="card-contain">
-          <div className="c-img-sec">
-            <img src="...../assets/DSC_3066-01.jpeg" alt="trip1" />
+        <div className="h-card-contain">
+          <div className="h-title">
+            Convert CSV to VCF
+          </div>
+          <div className="h-descrip">
+          Converting CSV to VCF using Python allows you to transfer contact information from spreadsheets to vCard files, which are widely used for importing contacts into phones and email clients. By utilizing libraries like `csv` and `vobject`, you can automate the conversion process, ensuring efficient and accurate data migration.
+          </div>
+          <div className="h-btn-contain">
+            <button className='h-card-btn'>Explore </button>
+          </div>
+        </div>
+        <div className="h-card-contain">
+          <div className="h-title">
+            Convert CSV to VCF
+          </div>
+          <div className="h-descrip">
+          Converting CSV to VCF using Python allows you to transfer contact information from spreadsheets to vCard files, which are widely used for importing contacts into phones and email clients. By utilizing libraries like `csv` and `vobject`, you can automate the conversion process, ensuring efficient and accurate data migration.
+          </div>
+          <div className="h-btn-contain">
+            <button className='h-card-btn'>Explore </button>
+          </div>
+        </div>
+        <div className="h-card-contain">
+          <div className="h-title">
+            Convert CSV to VCF
+          </div>
+          <div className="h-descrip">
+          Converting CSV to VCF using Python allows you to transfer contact information from spreadsheets to vCard files, which are widely used for importing contacts into phones and email clients. By utilizing libraries like `csv` and `vobject`, you can automate the conversion process, ensuring efficient and accurate data migration.
+          </div>
+          <div className="h-btn-contain">
+            <button className='h-card-btn'>Explore </button>
+          </div>
+        </div>
+      </div>
+      <div className="h-card-container">
+        <div className="h-card-contain">
+          <div className="h-title">
+            Convert CSV to VCF
+          </div>
+          <div className="h-descrip">
+          Converting CSV to VCF using Python allows you to transfer contact information from spreadsheets to vCard files, which are widely used for importing contacts into phones and email clients. By utilizing libraries like `csv` and `vobject`, you can automate the conversion process, ensuring efficient and accurate data migration.
+          </div>
+          <div className="h-btn-contain">
+            <button className='h-card-btn'>Explore </button>
+          </div>
+        </div>
+        <div className="h-card-contain">
+          <div className="h-title">
+            Convert CSV to VCF
+          </div>
+          <div className="h-descrip">
+          Converting CSV to VCF using Python allows you to transfer contact information from spreadsheets to vCard files, which are widely used for importing contacts into phones and email clients. By utilizing libraries like `csv` and `vobject`, you can automate the conversion process, ensuring efficient and accurate data migration.
+          </div>
+          <div className="h-btn-contain">
+            <button className='h-card-btn'>Explore </button>
+          </div>
+        </div>
+        <div className="h-card-contain">
+          <div className="h-title">
+            Convert CSV to VCF
+          </div>
+          <div className="h-descrip">
+          Converting CSV to VCF using Python allows you to transfer contact information from spreadsheets to vCard files, which are widely used for importing contacts into phones and email clients. By utilizing libraries like `csv` and `vobject`, you can automate the conversion process, ensuring efficient and accurate data migration.
+          </div>
+          <div className="h-btn-contain">
+            <button className='h-card-btn'>Explore </button>
           </div>
         </div>
       </div>
