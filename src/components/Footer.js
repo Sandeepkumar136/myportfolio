053d05a2ebdb9ec19f133f6406a30136bf745c0b
Sandeepkumar136 <ksundip750@gmail.com>
2024-09-03 23:18:19 +0530

    Footer Component Completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <div className='footer-container'>
         <div className="footer-title">
-            
+            Codewith Sanju &copy; 2024
         </div>
-      <ul className="footer-items"></ul>
+      <ul className="footer-items">
+        <li className="social-links"><a href="https://www.facebook.com/profile.php?id=61551699696823" className="social-hyper-link"><i class='footer-icons bx bxl-facebook'></i></a></li>
+        <li className="social-links"><a href="https://www.instagram.com/" className="social-hyper-link"><i class='footer-icons bx bxl-instagram' ></i></a></li>
+        <li className="social-links"><a href="https://t.me/CodewithSanju" className="social-hyper-link"><i class='footer-icons bx bxl-telegram' ></i></a></li>
+        <li className="social-links"><a href="https://github.com/Sandeepkumar136" className="social-hyper-link"><i class='footer-icons bx bxl-github' ></i></a></li>
+      </ul>
     </div>
   )
 }
